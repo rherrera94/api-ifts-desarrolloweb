c930dfedc4c5b10e8e2ff6ea30676d6fd7b99ff2
rherrera94 <rafael.herrera@outlook.com.ar>
2021-10-06 18:48:51 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,75 @@
+const jwt= require('jsonwebtoken');
+const unless=require('express-unless');
+const{SECRET_WORD}=require('./config/globals');
+const express= require('express');
+const cors=require ('cors');
+const app=express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+const {PORT}=require('./config/globals');
+
+/**
+ * AUTENTICACIÓN
+ */
+
+const auth=(req,res,next)=>{
+    try{
+        //nos fijamos si la request viene con un token
+        let token=req.headers['authorization'];
+        if (!token){//si no tiene token lanzamos error
+            throw new Error ("No ha iniciado sesión");
+        }
+        token= token.replace ('Bearer ', '');//hay que despejar el token para que quede solo
+        jwt.verify(token, SECRET_WORD, (err,user)=>{
+            if(err){//si hay algun problema con el token se lanza error
+                throw new Error("Token inválido");		
+            }
+        });
+        next();
+    }
+    catch(e){
+        res.status(403).send({"message": e.message});
+    }			 	
+   
+}
+
+/**
+ * Lo que se hace aca es que el usuario no va a poder entrar a ninguna ruta si
+ * es que no tiene un token valido (salvo a la ruta de login).
+ */
+
+auth.unless= unless;
+app.use(
+    auth.unless({
+        path:[
+            {url: '/userinterno/login', method: ['POST']}
+        ]
+    })
+)
+
+
+
+/**
+ * SE REQUIREN LOS DIFERENTES ARCHIVOS DE LAS RUTAS
+ */
+
+const routeUserInterno=require('./routes/rutasUserInterno');
+const routeEmpleado= require('./routes/rutasEmpleado');
+
+/*************************************************************************/
+/***
+ * RUTAS:
+ * userinterno: ruta de usuarios con alta, login,baja.
+ */
+
+app.use('/userinterno',routeUserInterno);
+app.use('/empleado',routeEmpleado);
+
+/*************************************************************************/
+app.use((req, res) => {
+    res.json({"error":"Ruta no encontrada","descripcion": `ruta ${req.originalUrl} no implementada` });
+})
+app.listen(PORT,()=>{
+    console.log('puerto '+ PORT);
+});
---
+++ routes/rutasEmpleado.js
@@ -0,0 +1,117 @@
+const express= require('express');
+const app=express.Router();
+const servicios= require('./services/servicesEmpleado')
+/**
+ * Agrega un nuevo Empleado a la tabla empleados.
+ * @returns {JSON} json que contiene el nuevo empleado agregado.
+ */
+app.post('/',async(req,res)=>{
+    try{
+        //controla que todos los campos necesarios vengan sin estar vacios
+        if(!req.body.cuil||!isNaN(req.body.cuil)||req.body.cuil.trim()==""||!req.body.apellido||!isNaN(req.body.apellido)
+        ||req.body.apellido.trim()==""|| !req.body.nombre||!isNaN(req.body.nombre)||req.body.nombre.trim()==""||
+        !req.body.mail||!isNaN(req.body.mail)||req.body.mail.trim()==""||!req.body.cargo||!isNaN(req.body.cargo)||
+        req.body.cargo.trim()==""){
+            throw new Error("Revise la información ingresada");
+        }
+        //me fijo si el empleado ya se encuentra registrado
+        let buscarxcuil=await servicios.cuilGetter(req.body.cuil);
+        if (buscarxcuil.length!=0){
+            throw new Error("El empleado ya se encuentra registrado");
+        }
+        
+        let empleadoReg={
+            "cuil":req.body.cuil,
+            "apellido":req.body.apellido.toUpperCase(),
+            "nombre":req.body.nombre.toUpperCase(),
+            "mail":req.body.mail.toUpperCase(),
+            "cargo":req.body.cargo.toUpperCase()
+        }
+        await servicios.empleadoIngreso (empleadoReg);
+        res.json(empleadoReg);
+    }catch(e){
+        if(e.message!="Revise la información ingresada" && e.message!="El empleado ya se encuentra registrado"){
+            res.status(404).json({"error":"Error inesperado"})
+            return;
+        }
+        res.status(404).json({"error":e.message});
+    }
+})
+
+/**
+ * Devuelve un listado general de todo los empleados que hay registrados 
+ * en la tabla de empleados con toda su información.
+ * @returns {JSON} json
+ */
+
+app.get('/',async (req,res)=>{
+    try{
+        let registros=await servicios.empleadosListado();
+        if (registros.length==0){
+            throw new Error ('No se han encontrado Empleados.');
+        }
+        res.status(200).send(registros);
+    }
+    catch(error){
+        if (error.message!='No se han encontrado Empleados.'){
+            res.status(413).send({"Mensaje":'error inesperado'});    
+        }
+        res.status(404).send({"Mensaje":error.message});
+    }
+})
+
+/*******************************************************************************/
+
+/**
+ * Devuelve la información del empleado que tiene número de id igual al que se 
+ * pasa por parámetro.
+ * @returns {JSON} json
+ */
+
+app.get('/:id',async (req,res)=>{
+    try{
+        let registros=await servicios.empleadoGetter(req.params.id);
+        if (registros.length==0){
+            throw new Error ('No se han encontrado empleados con ese id.');
+        } 
+        res.status(200).send(registros);
+    }
+    catch(error){
+        if(error.message!= 'No se han encontrado empleados con ese id.'){
+            res.status(413).send({"Mensaje": "error inesperado"});
+            return;    
+        }
+        res.status(404).send({"Mensaje": error.message});
+    }
+})
+
+/*******************************************************************************/
+
+/**
+ * Devuelve la información del empleado que tiene número de cuil igual al que se 
+ * pasa por parámetro.
+ * @returns {JSON} json
+ */
+
+app.get('/cuil/:cuil',async (req,res)=>{
+    try{
+        
+        let registros=await servicios.cuilGetter(req.params.cuil);
+        
+        if (registros.length==0){
+            throw new Error ('No se han encontrado empleados con ese cuil.');
+        } 
+        res.status(200).send(registros);
+    }
+    catch(error){
+        if(error.message!= 'No se han encontrado empleados con ese cuil.'){
+            res.status(413).send({"Mensaje": "error inesperado"});
+            return;    
+        }
+        res.status(404).send({"Mensaje": error.message});
+    }
+})
+
+/*******************************************************************************/
+
+module.exports=app;
